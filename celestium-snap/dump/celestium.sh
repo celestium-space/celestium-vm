d503d98b48695038eff0eb1aa15175ae05167564
hutli <jensstaermose@hotmail.com>
2021-07-07 18:51:29 +0200

    Persistent logging and persistent celestium cli binary

--- celestium-snap/dump/celestium.sh
+++ celestium-snap/dump/celestium.sh
@@ -1,15 +1,23 @@
-# !/bin/bash -ex
+/# !/bin/bash -ex
 sleep 10
-echo "\n\nStarting celestium" > /dev/tty1
+echo "\n\nStarting celestium" > /dev/tty1 > celestium.log
 # Generate 1000000 bytes of noice
 dd if=/dev/urandom bs=1000 count=1000 of=random
 
-echo "Downloading blocks file..." > /dev/tty1
-curl -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/blocks > blocks
-/snap/celestium/x1/celestium-cli -c mine -s blocks > /dev/tty1
+FILE=blocks
+if [ -f "$FILE" ]; then
+    echo "$FILE exists, skipping download" > /dev/tty1 > celestium.log
+else
+    echo "Downloading blocks file..." > /dev/tty1 > celestium.log
+    curl -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/blocks > blocks
+fi
+/snap/celestium/x1/celestium-cli -c mine -s blocks > /dev/tty1 > celestium.log
 
-echo "Uploading... " > /dev/tty1
+echo "Uploading... " > /dev/tty1 > celestium.log
 curl --upload-file random -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
-curl --upload-file mined_blocks -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
+curl --upload-file blocks -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
+curl --upload-file celestium.log -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
+echo "Deleting blocks file" > /dev/tty1 > celestium.log
+rm blocks
 
-echo "Done" > /dev/tty1
+echo "Done." > /dev/tty1 > celestium.log
