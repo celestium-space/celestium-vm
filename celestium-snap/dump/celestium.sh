de97b1bd189a01a6210293b099720daf21007744
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2021-07-08 17:14:20 +0200

    Saving worknig files and log to ubuntu home dir More easily accessed if MIS need to login

--- celestium-snap/dump/celestium.sh
+++ celestium-snap/dump/celestium.sh
@@ -1,23 +1,24 @@
 # !/bin/bash -ex
 sleep 10
-echo "\n\nStarting celestium" > /dev/tty1 > celestium.log
-# Generate 1000000 bytes of noice
-dd if=/dev/urandom bs=1000 count=1000 of=random
+echo "\n\nStarting celestium" | tee /dev/tty1 > /home/ubuntu/celestium.log
+echo "Generating 1000000 bytes of noice" | tee /dev/tty1 >> /home/ubuntu/celestium.log
+dd if=/dev/urandom bs=1000 count=1000 of=/home/ubuntu/random
 
 FILE=blocks
 if [ -f "$FILE" ]; then
-    echo "$FILE exists, skipping download" > /dev/tty1 > celestium.log
+    echo "$FILE exists, skipping download" | tee /dev/tty1 >> /home/ubuntu/celestium.log
 else
-    echo "Downloading blocks file..." > /dev/tty1 > celestium.log
-    curl -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/blocks > blocks
+    echo "Downloading blocks file..." | tee /dev/tty1 >> /home/ubuntu/celestium.log
+    curl -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/blocks > /home/ubuntu/blocks
 fi
-/snap/celestium/x1/celestium-cli -c mine -s blocks > /dev/tty1 > celestium.log
 
-echo "Uploading... " > /dev/tty1 > celestium.log
-curl --upload-file random -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
-curl --upload-file blocks -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
-curl --upload-file celestium.log -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
-echo "Deleting blocks file" > /dev/tty1 > celestium.log
-rm blocks
+/snap/celestium/x1/celestium-cli -c mine -s /home/ubuntu/blocks | tee /dev/tty1 >> /home/ubuntu/celestium.log
 
-echo "Done." > /dev/tty1 > celestium.log
+echo "Uploading... " | tee /dev/tty1 >> /home/ubuntu/celestium.log
+curl --upload-file /home/ubuntu/random -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
+curl --upload-file /home/ubuntu/blocks -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
+curl --upload-file /home/ubuntu/celestium.log -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
+echo "Deleting blocks file" | tee /dev/tty1 >> /home/ubuntu/celestium.log
+rm /home/ubuntu/blocks
+
+echo "Done." | tee /dev/tty1 >> /home/ubuntu/celestium.log/2
