a95ac758feec207ce16055153f5a6a9d34c5959a
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2021-07-15 18:25:42 +0200

    Extended logging for curl operations in celetium snap shell script

--- celestium-snap/dump/celestium.sh
+++ celestium-snap/dump/celestium.sh
@@ -9,15 +9,15 @@ if [ -f "$FILE" ]; then
     echo "$FILE exists, skipping download" | tee /dev/tty1 >> /home/ubuntu/celestium.log
 else
     echo "Downloading blocks file..." | tee /dev/tty1 >> /home/ubuntu/celestium.log
-    curl -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/blocks > /home/ubuntu/blocks
+    curl -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/blocks -o /home/ubuntu/blocks 2>&1 | tee /dev/tty1 >> celestium.log
 fi
 
 /snap/celestium/x1/celestium-cli -c mine -s /home/ubuntu/blocks | tee /dev/tty1 >> /home/ubuntu/celestium.log
 
 echo "Uploading... " | tee /dev/tty1 >> /home/ubuntu/celestium.log
-curl --upload-file /home/ubuntu/random -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
-curl --upload-file /home/ubuntu/blocks -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
-curl --upload-file /home/ubuntu/celestium.log -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/
+curl --upload-file /home/ubuntu/random -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/ 2>&1 | tee /dev/tty1 >> celestium.log
+curl --upload-file /home/ubuntu/blocks -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/ 2>&1 | tee /dev/tty1 >> celestium.log
+curl --upload-file /home/ubuntu/celestium.log -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/ 2>&1 | tee /dev/tty1 >> celestium.log
 echo "Deleting blocks file" | tee /dev/tty1 >> /home/ubuntu/celestium.log
 rm /home/ubuntu/blocks
 
