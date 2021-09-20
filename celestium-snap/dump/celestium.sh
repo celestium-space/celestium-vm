036d69afbfeec3066052128bd85b73fa912b9a12
hutli <jensstaermose@hotmail.com>
2021-09-21 00:29:44 +0200

    z-vector randomness generation

--- celestium-snap/dump/celestium.sh
+++ celestium-snap/dump/celestium.sh
@@ -1,8 +1,18 @@
 # !/bin/bash -ex
 sleep 10
 echo "\n\nStarting celestium" | tee /dev/tty1 > /home/ubuntu/celestium.log
-echo "Generating 1000000 bytes of noice" | tee /dev/tty1 >> /home/ubuntu/celestium.log
-dd if=/dev/urandom bs=1000 count=1000 of=/home/ubuntu/random
+
+mkdir /home/ubuntu/images
+
+for I in 1 .. 10
+do
+	echo "Downloading noice image $I" | tee /dev/tty1 >> /home/ubuntu/celestium.log
+    curl -u 'WORKGROUP\MISOWNER:00000000' smb://MIS-PL-AM12/shared/{$I}.jpg -o /home/ubuntu/images/{$I}.jpg 2>&1 | tee /dev/tty1 >> celestium.log
+done
+
+echo "Generating random noice" | tee /dev/tty1 >> /home/ubuntu/celestium.log
+/snap/celestium/x1/celestium-cli -c z-vector -s /home/ubuntu/images | tee /dev/tty1 >> /home/ubuntu/celestium.log
+#dd if=/dev/urandom bs=1000 count=1000 of=/home/ubuntu/random
 
 FILE=/home/ubuntu/blocks
 if [ -f "$FILE" ]; then
